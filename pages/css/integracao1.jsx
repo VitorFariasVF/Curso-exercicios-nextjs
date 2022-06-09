49efffbd2051119ef6719d1fedeb0c94640cf1a2
Vitor Farias <joaovitor1081@gmail.com>
2022-06-09 16:06:25 -0300

    css01

---
+++ pages/css/integracao1.jsx
@@ -0,0 +1,12 @@
+// Não posso importar aqui!
+// import foi feito dentro de _app.js
+
+export default function integracao() {
+    return (
+        <div className="integracao1">
+            <div className="vermelha">Texto #01</div>
+            <div className="azul">Texto #02</div>
+            <duv className="branco">Texto #03</duv>
+        </div>
+    )
+}
